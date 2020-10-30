dc929b0319b519cfb123a5e3b7f15a7d5365d689
Filip Zmiša <fz515824@fer.hr>
2020-10-30 19:53:07 +0100

    napravljen basic Django server s privremenim frontendom

---
+++ izvorniKod/djangoServer/maketashop/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+
+from . import views
+
+#app_name ='maketashop'
+urlpatterns = [
+    path('', views.index, name='index'),
+    #path('index/', views.index, name='index'),
+    path('cart/', views.cart, name='cart'),
+    path('login/', views.login, name='login'),
+    path('profil/', views.profil, name='profil'),
+    path('signup/', views.signup, name='signup'),
+    path('webshop/', views.webshop, name='webshop'),
+]
---
+++ izvorniKod/djangoServer/maketashop/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def index(request):
+    return render(request, 'maketashop/index.html')
+
+def cart(request):
+    return render(request, 'maketashop/cart.html')
+
+def login(request):
+    return render(request, 'maketashop/login.html')
+
+def profil(request):
+    return render(request, 'maketashop/profil.html')
+
+def signup(request):
+    return render(request, 'maketashop/signup.html')
+
+def webshop(request):
+    return render(request, 'maketashop/WebShop.html')
