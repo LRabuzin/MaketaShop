69506dd53df8daa9e9013dcb0b6e6962cbeb5047
Filip Zmiša <fz515824@fer.hr>
2020-11-02 15:12:03 +0100

    postavke za postgres i view editovi

--- izvorniKod/djangoServer/WeTried/settings.py
+++ izvorniKod/djangoServer/WeTried/settings.py
@@ -74,10 +74,23 @@ WSGI_APPLICATION = 'WeTried.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': 'mydatabase',
+#    }
+#}
+
+#ovo je za POSTGRESQL
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'weTriedDatabase',
+        'USER': 'admin',
+        'PASSWORD': 'wetried',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
--- izvorniKod/djangoServer/maketashop/views.py
+++ izvorniKod/djangoServer/maketashop/views.py
@@ -3,40 +3,40 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return render(request, 'maketashop/index.html')
+    return render(request, 'maketashop/index.html', {'title': index, 'link_active': index, 'empty_head': False})
 
 def cart(request):
-    return render(request, 'maketashop/cart.html')
+    return render(request, 'maketashop/cart.html', {'title': cart, 'link_active': cart, 'empty_head': False})
 
 def login(request):
-    return render(request, 'maketashop/login.html')
+    return render(request, 'maketashop/login.html', {'title': login, 'link_active': login, 'empty_head': False})
 
 def profil(request):
-    return render(request, 'maketashop/profil.html')
+    return render(request, 'maketashop/profil.html', {'title': profil, 'link_active': profil, 'empty_head': False})
 
 def signup(request):
-    return render(request, 'maketashop/signup.html')
+    return render(request, 'maketashop/signup.html', {'title': signup, 'link_active': signup, 'empty_head': False})
 
 def webshop(request):
-    return render(request, 'maketashop/WebShop.html')
+    return render(request, 'maketashop/WebShop.html', {'title': webshop, 'link_active': webshop, 'empty_head': False})
 
 def b_post(request):
-    return render(request, 'maketashop/b_post.html')
+    return render(request, 'maketashop/b_post.html', {'title': b_post, 'link_active': b_post, 'empty_head': False})
 
 def checkout(request):
-    return render(request, 'maketashop/checkout.html')
+    return render(request, 'maketashop/checkout.html', {'title': checkout, 'link_active': checkout, 'empty_head': False})
 
 def maketa(request):
-    return render(request, 'maketashop/maketa.html')
+    return render(request, 'maketashop/maketa.html', {'title': maketa, 'link_active': maketa, 'empty_head': False})
     
 def pregledKorisnika(request):
-    return render(request, 'maketashop/pregledKorisnika.html')
+    return render(request, 'maketashop/pregledKorisnika.html', {'title': pregledKorisnika, 'link_active': pregledKorisnika, 'empty_head': False})
 
 def transakcije(request):
-    return render(request, 'maketashop/transakcije.html')
+    return render(request, 'maketashop/transakcije.html', {'title': transakcije, 'link_active': transakcije, 'empty_head': False})
 
 def inbox(request):
-    return render(request, 'maketashop/inbox.html')
+    return render(request, 'maketashop/inbox.html', {'title': inbox, 'link_active': inbox, 'empty_head': False})
 
 def interakcija(request):
-    return render(request, 'maketashop/interakcija.html')
+    return render(request, 'maketashop/interakcija.html', {'title': interakcija, 'link_active': interakcija, 'empty_head': False})
