dfdeccabeaac672b10021ed97642eca2b78c2835
Filip Zmiša <fz515824@fer.hr>
2020-11-07 18:56:35 +0100

    dodan komentar

--- izvorniKod/djangoServer/maketashop/views/B_PostView.py
+++ izvorniKod/djangoServer/maketashop/views/B_PostView.py
@@ -3,6 +3,7 @@ from django.views.generic import View
 from django.shortcuts import render
 from maketashop.models import Prica
 from maketashop.models import Napravljenaod
+from maketashop.models import Komentar
 
 class B_Post(View):
     template_name ="maketashop/b_post.html"
@@ -10,10 +11,12 @@ class B_Post(View):
         # <view logic>
         prica = Prica.objects.select_related().get(pricaid=id)
         napod = Napravljenaod.objects.get(maketaid=prica.maketaid)
+        koment = Komentar.objects.get(pricaid=prica.pricaid)
         return render(request, self.template_name, {
             'title': "b_post", 
             'link_active': "b_post", 
             'empty_head': False,
             'baza_data': prica,
-            'baza_data_2': napod
+            'baza_data_2': napod,
+            'baza_data_3': koment,
             })
