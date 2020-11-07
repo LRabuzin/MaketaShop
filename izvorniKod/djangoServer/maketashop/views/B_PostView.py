edcc05a7db4e85c47db2b82ac101753f35b7d757
Filip Zmiša <fz515824@fer.hr>
2020-11-07 18:41:17 +0100

    pls radi

--- izvorniKod/djangoServer/maketashop/views/B_PostView.py
+++ izvorniKod/djangoServer/maketashop/views/B_PostView.py
@@ -2,15 +2,18 @@ from django.http import HttpResponse
 from django.views.generic import View
 from django.shortcuts import render
 from maketashop.models import Prica
+from maketashop.models import Napravljenaod
 
 class B_Post(View):
     template_name ="maketashop/b_post.html"
     def get(self, request, id):
         # <view logic>
         prica = Prica.objects.select_related().get(pricaid=id)
+        napod = Napravljenaod.objects.get(maketaid=prica.maketaid)
         return render(request, self.template_name, {
             'title': "b_post", 
             'link_active': "b_post", 
             'empty_head': False,
-            'baza_data': prica
+            'baza_data': prica,
+            'baza_data_2': napod
             })
