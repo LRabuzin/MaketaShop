2691c0514540b8f10a9ede3b9e8ef80fd8eb7675
Leon Novacki <leon.novacki@fer.hr>
2020-11-07 20:39:05 +0100

    Dodao postavke privatnosti

--- izvorniKod/djangoServer/maketashop/views/ProfilView.py
+++ izvorniKod/djangoServer/maketashop/views/ProfilView.py
@@ -1,6 +1,7 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.views.generic import View
 from django.shortcuts import render
+from django.urls import reverse
 from ..forms import PrivacyForm
 from maketashop.models import Korisnik
 
@@ -8,9 +9,16 @@ class Profil(View):
     template_name ="maketashop/profil.html"
     def get(self, request):
         # <view logic>
-        form = PrivacyForm()
-        
         korisnik = Korisnik.objects.get(email=request.session['user'])
+
+        data = {'address' : korisnik.adresaprivatna,
+                'register_date' : korisnik.datumregistracijeprivatan,
+                'birth_date' : korisnik.rodendanprivatan}
+                #picture
+                #surname
+                #email
+        form = PrivacyForm(data)
+
         return render(request, self.template_name, {
             'title': "profil", 
             'link_active': "profil", 
@@ -19,3 +27,21 @@ class Profil(View):
             'baza_data': korisnik,
             'session': request.session
             })
+
+    def post(self, request):
+        # <view logic>
+        form = PrivacyForm(request.POST)
+        if form.is_valid():
+            adresaprivatna = form.cleaned_data['address']
+            registerprivatan = form.cleaned_data['register_date']
+            rodendanprivatan = form.cleaned_data['birth_date']
+            #picture
+            #surname
+            #email
+            korisnik = Korisnik.objects.get(email=request.session['user'])
+            korisnik.adresaprivatna = adresaprivatna
+            korisnik.datumregistracijeprivatan = registerprivatan
+            korisnik.rodendanprivatan = rodendanprivatan
+            korisnik.save()
+
+            return HttpResponseRedirect(reverse('profil'))
