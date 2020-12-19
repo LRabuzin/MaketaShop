fa091ae62e9537d9be3e637dea6a6fcfabe4120c
Filip Zmiša <fz515824@fer.hr>
2020-12-19 15:32:24 +0100

    webshopDTO dopunjen

--- izvorniKod/djangoServer/maketashop/DTOs/WebShopDTO.py
+++ izvorniKod/djangoServer/maketashop/DTOs/WebShopDTO.py
@@ -1,7 +1,12 @@
 from maketashop.models import Maketa
+from maketashop.models import Napravljenaod
 
 class WebShopDTO():
     def __init__(self):
         self.makete=Maketa.objects.all().select_related()
+        self.rijecnikMaketaMaterijaliCijene={}
+        for maketa in self.makete:
+            napravljenaOd=Napravljenaod.objects.filter(maketaid=maketa.maketaid)
+            self.rijecnikMaketaMaterijaliCijene[maketa]=napravljenaOd
     def getMakete(self):
-        return self.makete
+        return self.rijecnikMaketaMaterijaliCijene
