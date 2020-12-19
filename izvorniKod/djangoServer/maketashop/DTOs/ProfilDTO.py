ebd6cc311f4d829e713716c08d3d10b8f5a81f4b
Filip Zmiša <fz515824@fer.hr>
2020-12-19 12:46:33 +0100

    Fixao LEKIJEVE tabove

--- izvorniKod/djangoServer/maketashop/DTOs/ProfilDTO.py
+++ izvorniKod/djangoServer/maketashop/DTOs/ProfilDTO.py
@@ -5,11 +5,11 @@ class ProfilDTO():
    def __init__(self, korisnikId):
       self.korisnik=Korisnik.objects.get(korisnikId)
 
-	def getEmail(self):
-		return self.korisnik.email
-	
-	def getKorisnickoIme(self):
-		return self.korisnik.korisnickoime
+   def getEmail(self):
+      return self.korisnik.email
+
+   def getKorisnickoIme(self):
+      return self.korisnik.korisnickoime
 
    def getAdresa(self):
       return self.korisnik.adresa
@@ -20,23 +20,14 @@ class ProfilDTO():
    def getDatumRegistracije(self):
       return self.korisnik.datumregistracije
 
-	def getIme(self):
-		return self.korisnik.ime
+   def getIme(self):
+      return self.korisnik.ime
 
-	def getPrezime(self):
-		return self.korisnik.prezime
+   def getPrezime(self):
+      return self.korisnik.prezime
 
-	def getSlika(self):
-		return self.korisnik.profilnaid.putdodatoteke
+   def getSlika(self):
+      return self.korisnik.profilnaid.putdodatoteke
       
    def getKorisnik(self):
       return self.korisnik.korisnickoime
-
-
-
-
-
-
-
-
-
