a0051eb2252472f5da773bc41ab17dd31cf07f89
Filip Zmiša <fz515824@fer.hr>
2020-12-20 11:31:21 +0100

    dodan CartItem i improvan CartDTO

--- izvorniKod/djangoServer/maketashop/DTOs/CartDTO.py
+++ izvorniKod/djangoServer/maketashop/DTOs/CartDTO.py
@@ -1,5 +1,24 @@
 from maketashop.models import Maketa
 
+class CartItem():
+    def __init__(self, maketaID, materijal, cijena):
+        self.maketa=Maketa.objects.select_related.get(maketaid=maketaID)
+        self.materijal=materijal
+        self.cijena=cijena
+
+    def getMaketa(self):
+        return self.maketa
+
+    def getMaterijal(self):
+        return self.materijal
+    
+    def getCijena(self):
+        return self.cijena
+
+    def __eq__(self, obj):
+        return isinstace(obj, CartItem) and obj.maketa == self.maketa and obj.materijal == self.materijal and obj.cijena == self.cijena
+
+
 class CartDTO():
     def __init__(self):
         self.inventory={}
@@ -7,15 +26,15 @@ class CartDTO():
     def getCart(self):
         return self.inventory
 
-    def addMaketa(self, maketaID, kolicina):
-        maketa = Maketa.objects.select_related.get(maketaid=maketaID)
+    def addMaketa(self, maketaID, materijal, cijena, kolicina):
+        maketa = CartItem(Maketa.objects.select_related.get(maketaid=maketaID), materijal, cijena)
         if maketa in self.inventory:
             self.inventory[maketa]=self.inventory[maketa]+kolicina
         else:
             self.inventory[maketa]=kolicina
 
-    def removeMaketa(self, maketaID, kolicina):
-        maketa = Maketa.objects.select_related.get(maketaid=maketaID)
+    def removeMaketa(self, maketaID, materijal, cijena, kolicina):
+        maketa = CartItem(Maketa.objects.select_related.get(maketaid=maketaID), materijal, cijena)
         if maketa in self.inventory:
             self.inventory[maketa]=self.inventory[maketa]-kolicina
         if self.inventory[maketa]<=0:
