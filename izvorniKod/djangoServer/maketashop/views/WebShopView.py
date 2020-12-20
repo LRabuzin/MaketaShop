7a322bf58ad305a6a4a62c2be1a537731ed66830
Filip Zmiša <fz515824@fer.hr>
2020-12-20 12:18:03 +0100

    dodan post u WebShopView

--- izvorniKod/djangoServer/maketashop/views/WebShopView.py
+++ izvorniKod/djangoServer/maketashop/views/WebShopView.py
@@ -1,7 +1,9 @@
 from django.http import HttpResponse
 from django.views.generic import View
 from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect
 from maketashop.DTOs.WebShopDTO import WebShopDTO
+from maketashop.DTOs.CartDTO import CartDTO
 
 class WebShop(View):
     template_name ="maketashop/WebShop.html"
@@ -14,3 +16,15 @@ class WebShop(View):
             'webShopDTO': WebShopDTO(),
             'session': request.session
             })
+
+    def post(self, request):
+        if maketaId in request.POST:
+            maketaID = request.POST['maketaId']
+            cijena = request.POST['cijena']
+            materijal = request.POST['materijal']
+
+            if not 'cart' in request.session:
+                request.session['cart']=CartDTO()
+            request.session['cart'].addMaketa(maketaID, cijena, materijal, 1)
+        else:
+             return HttpResponseRedirect(reverse('webshop'))
