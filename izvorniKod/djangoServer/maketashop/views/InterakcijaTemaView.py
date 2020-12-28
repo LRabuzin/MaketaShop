53264671eca41099364fe59883550b1cdfaa740c
Tvrtko Hrestak <th51372@fer.hr>
2020-12-28 19:24:25 +0100

    Interakcija tema edit

--- izvorniKod/djangoServer/maketashop/views/InterakcijaTemaView.py
+++ izvorniKod/djangoServer/maketashop/views/InterakcijaTemaView.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse
 from django.views.generic import View
 from maketashop.models import Interakcija
+from maketashop.models import Korisnik
 from maketashop.DTOs.InterakcijaTemaDTO import InterakcijaTemaDTO
 
 class InterakcijaTema(View):
@@ -11,14 +12,16 @@ class InterakcijaTema(View):
         if 'user' not in request.session:
             return HttpResponseRedirect(reverse('login'))
         else:
-            if request.session['user'].jeadmin :
-                return render(request, self.template_name, {
-                    'title': "interakcija", 
-                    'link_active': "interakcija", 
-                    'empty_head': False,
-                    'InterakcijaDTO' : InterakcijaTemaDTO(id), 
-                    'session': request.session
-                    })
+            user = Korisnik.objects.select_related().get(email = request.session['user'])
+            if user.jeadmin:
+               return render(request, self.template_name, {
+                  'title': "interakcija", 
+                  'link_active': "interakcija", 
+                  'empty_head': False,
+                  'InterakcijaDTO' : InterakcijaTemaDTO(id), 
+                  'jeAdmin' : user.jeadmin,
+                  'session': request.session
+                  })
             else:
                 interakcija = Interakcija.objects.select_related().get(interakcijaid = id)
 
