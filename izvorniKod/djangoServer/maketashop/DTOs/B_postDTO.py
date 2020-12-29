13d68f9782da3f9be4856b9d83a7c1857a9aef57
Filip Zmiša <fz515824@fer.hr>
2020-12-29 17:40:17 +0100

    b post pocetak popravaka (losi dto)

--- izvorniKod/djangoServer/maketashop/DTOs/B_postDTO.py
+++ izvorniKod/djangoServer/maketashop/DTOs/B_postDTO.py
@@ -12,6 +12,35 @@ class B_postDTO():
         self.lajk = lajkao;
         self.brojLajkova = Prica.getBrojLajkova;
         self.brojDislajkova = Prica.getBrojDislajkova;
+
+        #self.prica=Prica.objects.select_related().get(pricaid=pricaID)
+        medijaprice=Multimedijaprice.objects.select_related().filter(pricaid=self.prica.pricaid)
+        self.slika=None
+        self.tekst=None
+
+        for medija in medijaprice:
+            minslika=None
+            mintekst=None
+            if medija.mediaid.vrstamedije=='slika':
+                if minslika==None:
+                    minslika=medija.poredakuprici
+                if minslika>=medija.poredakuprici:
+                    minslika=medija.poredakuprici
+                    self.slika=medija.mediaid.putdodatoteke
+                
+            if medija.mediaid.vrstamedije=='tekst':
+                if mintekst==None:
+                    mintekst=medija.poredakuprici
+                if mintekst>=medija.poredakuprici:
+                    mintekst=medija.poredakuprici
+                    self.tekst=medija.mediaid.putdodatoteke
+        
+    def getSlika(self):
+        return self.slika
+    
+    def getTekst(self):
+        return self.tekst
+
     def getPrica(self):
         return self.prica
     def getNapod(self):
--- izvorniKod/djangoServer/maketashop/views/CheckoutView.py
+++ izvorniKod/djangoServer/maketashop/views/CheckoutView.py
@@ -12,95 +12,99 @@ class Checkout(View):
     def get(self, request):
         # <view logic>
         email = request.session.get('user')
-        if email:
-            korisnik = ProfilDTO(email)
-            kartica = CreditCardDTO(email)
+        if 'cart' in request.session:
+            if email:
+                korisnik = ProfilDTO(email)
+                kartica = CreditCardDTO(email)
 
-            data = {
-                'ime' : korisnik.getIme(),
-                'prezime' : korisnik.getPrezime(),
-                'adresa' : korisnik.getAdresa(),
-                'email' : korisnik.getEmail(),
-                'ime_na_kartici': kartica.getKKImePrezime(),
-                'paypal_bool': 'paypal' if kartica.getKKPaypal() else 'kreditnaKartica',
-                'broj_kartice': kartica.getKKBroj(),
-                'istek_kartice': kartica.getKKIstek(),
-            }
-            form = PlacanjeForm(data)
-            return render(request, self.template_name, {
-                'title': "checkout",
-                'link_active' : "checkout",
-                'empty_head' : False,
-                'form' : form,
-                'session' : request.session
-            })
+                data = {
+                    'ime' : korisnik.getIme(),
+                    'prezime' : korisnik.getPrezime(),
+                    'adresa' : korisnik.getAdresa(),
+                    'email' : korisnik.getEmail(),
+                    'ime_na_kartici': kartica.getKKImePrezime(),
+                    'paypal_bool': 'paypal' if kartica.getKKPaypal() else 'kreditnaKartica',
+                    'broj_kartice': kartica.getKKBroj(),
+                    'istek_kartice': kartica.getKKIstek(),
+                }
+                form = PlacanjeForm(data)
+                return render(request, self.template_name, {
+                    'title': "checkout",
+                    'link_active' : "checkout",
+                    'empty_head' : False,
+                    'form' : form,
+                    'session' : request.session
+                })
 
+            else:
+                form = PlacanjeForm()
+
+                return render(request, self.template_name, {
+                    'title': "checkout", 
+                    'link_active': "checkout", 
+                    'empty_head': False,
+                    'form' : form,
+                    'session': request.session
+                })
         else:
-            form = PlacanjeForm()
-
-            return render(request, self.template_name, {
-                'title': "checkout", 
-                'link_active': "checkout", 
-                'empty_head': False,
-                'form' : form,
-                'session': request.session
-            })
-
+            return HttpResponseRedirect(reverse('cart'))
     def post(self, request):
-        form = PlacanjeForm(request.POST)
-        if form.is_valid():
-            ime = form.cleaned_data['ime']
-            prezime = form.cleaned_data['prezime']
-            adresa = form.cleaned_data['adresa']
-            email = form.cleaned_data['email']
-            ime_na_kartici = form.cleaned_data['ime_na_kartici']
-            broj_kartice = form.cleaned_data['broj_kartice']
-            istek_kartice = form.cleaned_data['istek_kartice']
-            paypal_bool = form.cleaned_data['paypal_bool']
+        if 'cart' in request.session:
+            form = PlacanjeForm(request.POST)
+            if form.is_valid():
+                ime = form.cleaned_data['ime']
+                prezime = form.cleaned_data['prezime']
+                adresa = form.cleaned_data['adresa']
+                email = form.cleaned_data['email']
+                ime_na_kartici = form.cleaned_data['ime_na_kartici']
+                broj_kartice = form.cleaned_data['broj_kartice']
+                istek_kartice = form.cleaned_data['istek_kartice']
+                paypal_bool = form.cleaned_data['paypal_bool']
 
-            cart = request.session['cart']
-            ukupaniznos = cart.getUkupno()
-            
-            if 'user' in request.session:
-                korisnik = Korisnik.objects.get(email=request.session['user'])
-                if(korisnik.kkimeprezime!=ime_na_kartici):
-                    korisnik.kkimeprezime=ime_na_kartici
-
-                if(korisnik.kkpaypal == False):
-                    if(paypal_bool=='paypal'):
-                        korisnik.kkpaypal=True
-                else:
-                    if(paypal_bool=='kreditnaKartica'):
-                        korisnik.kkpaypal=False
+                cart = request.session['cart']
+                ukupaniznos = cart.getUkupno()
                 
-                if(korisnik.kkbroj != broj_kartice):
-                    korisnik.kkbroj = broj_kartice
+                if 'user' in request.session:
+                    korisnik = Korisnik.objects.get(email=request.session['user'])
+                    if(korisnik.kkimeprezime!=ime_na_kartici):
+                        korisnik.kkimeprezime=ime_na_kartici
 
-                if(korisnik.kkistek != istek_kartice):
-                    korisnik.kkistek = istek_kartice
+                    if(korisnik.kkpaypal == False):
+                        if(paypal_bool=='paypal'):
+                            korisnik.kkpaypal=True
+                    else:
+                        if(paypal_bool=='kreditnaKartica'):
+                            korisnik.kkpaypal=False
+                    
+                    if(korisnik.kkbroj != broj_kartice):
+                        korisnik.kkbroj = broj_kartice
 
-                korisnik.save()
+                    if(korisnik.kkistek != istek_kartice):
+                        korisnik.kkistek = istek_kartice
 
-            novaTransakcija = Transakcija()
-            novaTransakcija.ime = ime
-            novaTransakcija.prezime = prezime
-            novaTransakcija.adresa = adresa
-            novaTransakcija.brojracuna = broj_kartice
-            novaTransakcija.ukupaniznos = ukupaniznos
-            if 'user' in request.session:
-                novaTransakcija.korisnik = Korisnik.objects.get(email=request.session['user'])
-            novaTransakcija.save()
+                    korisnik.save()
 
-            for maketa,kol in cart.getCart().items():
-                maketakupljena = Maketakupljena()
-                maketakupljena.maketaid = maketa.getMaketa()
-                maketakupljena.materijalid = Materijal.objects.get(ime=maketa.getMaterijal())
-                maketakupljena.kolicina = kol
-                maketakupljena.transakcijaid = novaTransakcija
-                maketakupljena.save()
-            
-            if 'cart' in request.session:
-                del request.session['cart']
+                novaTransakcija = Transakcija()
+                novaTransakcija.ime = ime
+                novaTransakcija.prezime = prezime
+                novaTransakcija.adresa = adresa
+                novaTransakcija.brojracuna = broj_kartice
+                novaTransakcija.ukupaniznos = ukupaniznos
+                if 'user' in request.session:
+                    novaTransakcija.korisnik = Korisnik.objects.get(email=request.session['user'])
+                novaTransakcija.save()
 
-            return HttpResponseRedirect(reverse('transakcije'))
-        return HttpResponseRedirect(self.request.path_info)
+                for maketa,kol in cart.getCart().items():
+                    maketakupljena = Maketakupljena()
+                    maketakupljena.maketaid = maketa.getMaketa()
+                    maketakupljena.materijalid = Materijal.objects.get(ime=maketa.getMaterijal())
+                    maketakupljena.kolicina = kol
+                    maketakupljena.transakcijaid = novaTransakcija
+                    maketakupljena.save()
+                
+                if 'cart' in request.session:
+                    del request.session['cart']
+
+                return HttpResponseRedirect(reverse('transakcije'))
+            return HttpResponseRedirect(self.request.path_info)
+        return HttpResponseRedirect(reverse('index'))
